59b4aeea2f491902073a38f2a50f948b69a70b43
Camila Solessio <solessio.camila@gmail.com>
2021-10-05 13:55:46 -0300

    final2

--- js/main.js
+++ js/main.js
@@ -1,55 +1,128 @@
+//OBJETOS
+
+/* Crear nuevos productos y agregarlos al stock (productos[]) */
+
+class productosNuevos {
+  constructor(id,nombre, precio, stock) {
+      this.id = id;
+      this.nombre = `Zapatillas ${nombre}`;
+      this.precio = Number(precio);
+      this.stock = stock;
+
+  }
+
+}
+
+let sky = new productosNuevos (5, 'Oxford', 12300, 7);
+productos.push(sky);
+let savy = new productosNuevos (6, 'Savy', 11200, 3);
+productos.push(savy);
+let raven = new productosNuevos(7,'Raven',9500,5);
+productos.push(raven);
 
 
-//elegirProductos();
+
+//FUNCIONES
+
+
+//mostrar productos por dom
+const mostrarProductos = (array) => {
     
-/* Si se realiza una compra */
+  for (const producto of array) {
+      let contenedor = document.querySelector('#productosContainer')
+      let newDiv= document.createElement('div');
+      newDiv.className = "productos";
+      newDiv.innerHTML = 
+      `<img class='fotoProducto' src='imagenes/fotoProducto${producto.id}.jpeg'>
+      <h3> ${producto.nombre}</h3>
+      <p> $${producto.precio}</p>
+      <button class='btnAgregar' id='btn${producto.id}Agregar'>agregar</button>`;
+      contenedor.appendChild(newDiv);
 
-// if (total > 0) {
+  }
+}
 
-//     /* tira resumen de la misma con cada articulo, cantidad y su subtotal de cada uno. 
-//     Pregunta si es correcto */
+//agregar productos al carrito y mostrarlos en dom 
+const agregarProductos = () => {
+  productos.forEach((producto)=>{
+      $(`#btn${producto.id}Agregar`).on('click', function () {
+          $(`<p class='p' id='p'> 1 ${producto.nombre} por $${producto.precio} agregado al carrito</p>`).insertBefore('#resumenCompra')
+          
+          cart.push(producto.precio);
+          $('#resumenCompra').show();
 
-//     resumen();
+          let suma = 0;
+          let mostrar = 0;
+          $('#totalCompra').empty()
 
-//     /* si no es correcto, vuelve a iniciar la compra */
+          cart.forEach((elementos)=> {
+              suma += elementos;
+          })
+          mostrar = $('#totalCompra').append(`Tu compra es de $${suma}`);
+          return mostrar
+      });
+  })
+}
 
-//     while (finCompra === 'n' || finCompra === 'N'){
-//         totales = [];
-//         resumenCompra = [];
-//         elegirProductos ();
-//         resumen();
-//     }
-    
 
-//     let tuCompra = document.createElement('p');
-//     tuCompra.className = ('tuCompra');
-//     tuCompra.innerHTML = `Tu compra es de:`
-//     document.getElementById('resumenCompra').appendChild(tuCompra); 
+//boton para finalizar la compra 
+const finalizarCompra = () => {
+  $('.btnCompra').on('click', function (){
+    $('#mensajeFinal').append(`<p>Gracias por tu compra!</p>`);
+    $('#cart').remove();
+  })
+  
+}
 
-//     /* crea elementos p con resumen de la compra (de array resumenCompra) y los agrega al div id resumenCompra*/
-//     for (const producto of resumenCompra) {
-//         let resumen = document.createElement('p');
-//         resumen.className = 'elementoResumen';
-//         resumen.innerHTML = producto.slice(0, -19);
-//         document.getElementById('resumenCompra').appendChild(resumen);
-//     }
-
-//     /* agrega elemento <p> con monto final de la compra */
-//     let pagoFinal = document.createElement('p');
-//     pagoFinal.className = 'pagoFinal';
-//     pagoFinal.innerHTML = `
-//     Tu pago final es de $${sumarTotales()}`;
-//     document.getElementById('resumenCompra').appendChild(pagoFinal);
-    
-// }
-
-mostrarProductos(productos);
-
+//BOTON PARA MODO DARK
 let btnModo = document.getElementById('btnDark');
 btnModo.onclick = () => {
-  document.body.classList.toggle('dark');
-  btnDark.classList.toggle('active');
+document.body.classList.toggle('dark');
+btnDark.classList.toggle('active');
 }
+
+
+//llamar funciones
+
+$('#resumenCompra').hide();
+
+mostrarProductos(productos);
+agregarProductos();
+finalizarCompra();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
